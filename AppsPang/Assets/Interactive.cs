d9b3605daf860c15cd6a6fe6bdb4b7be07b95d3f
l_ms_oh_ <tkfkdgo00102@naver.com>
2020-07-04 18:25:48 +0900

    마우스 클릭 확인
    2020.07.04
    마우스 클릭시 "Input ! + 마우스 위치" 출력.

---
+++ AppsPang/Assets/Interactive.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactive : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Input!"+Input.mousePosition);
+        }
+    }
+}
