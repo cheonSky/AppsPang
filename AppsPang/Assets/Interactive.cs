939ed76aaedd88519869974478d8eb8cc83f858d
l_ms_oh_ <tkfkdgo00102@naver.com>
2020-07-05 19:57:21 +0900

    드래그 방향 확인
    2020.07.05
    
    대각선으로 드래그시 올바른 판정 체크.
    ※ 움직인 거리의 절대값을 통한 방향 설정.

--- AppsPang/Assets/Interactive.cs
+++ AppsPang/Assets/Interactive.cs
@@ -4,11 +4,44 @@ using UnityEngine;
 
 public class Interactive : MonoBehaviour
 {
+    public RectTransform _image;
+    private Vector2 mousePosition1;
+
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if(Input.GetMouseButtonDown(0))
         {
-            Debug.Log("Input!"+Input.mousePosition);
+            Vector2 mousePosition = Input.mousePosition;
+
+            if(Vector2.Distance(_image.position, mousePosition) < 50)
+            {
+                mousePosition1 = mousePosition;
+            }
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            Vector2 mousePosition2 = Input.mousePosition;
+            Vector2 mouseMove = (mousePosition1 - mousePosition2);
+            float X = Mathf.Abs(mouseMove.x);
+            float Y = Mathf.Abs(mouseMove.y);
+
+            if(mouseMove.y < 0 && X < Y)
+            {
+                Debug.Log("Up");
+            }
+            else if(mouseMove.y > 0 && X < Y)
+            {
+                Debug.Log("Down");
+            }
+            else if(mouseMove.x > 0 && X > Y)
+            {
+                Debug.Log("Left");
+            }
+            else if(mouseMove.x < 0 && X > Y)
+            {
+                Debug.Log("Right");
+            }
         }
     }
 }
