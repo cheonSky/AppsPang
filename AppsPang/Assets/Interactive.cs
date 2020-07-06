881b6ca8337e8108f68fb7ca5aa3fe029203bbb5
l_ms_oh_ <tkfkdgo00102@naver.com>
2020-07-06 20:06:22 +0900

    물체 클릭 확인
    2020.07.06
    
    빈공간에서도 드래그 되는 오류 발견.
    물체를 드래그시에만 판정 되도록 수정.

--- AppsPang/Assets/Interactive.cs
+++ AppsPang/Assets/Interactive.cs
@@ -6,42 +6,49 @@ public class Interactive : MonoBehaviour
 {
     public RectTransform _image;
     private Vector2 mousePosition1;
+    private int check;
 
     private void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))
         {
             Vector2 mousePosition = Input.mousePosition;
 
-            if(Vector2.Distance(_image.position, mousePosition) < 50)
+            if (Vector2.Distance(_image.position, mousePosition) < 50)
             {
                 mousePosition1 = mousePosition;
+                check = 1;
             }
         }
 
         if (Input.GetMouseButtonUp(0))
         {
-            Vector2 mousePosition2 = Input.mousePosition;
-            Vector2 mouseMove = (mousePosition1 - mousePosition2);
-            float X = Mathf.Abs(mouseMove.x);
-            float Y = Mathf.Abs(mouseMove.y);
+            if (check == 1)
+            {
+                Vector2 mousePosition2 = Input.mousePosition;
+                Vector2 mouseMove = (mousePosition1 - mousePosition2);
+                float X = Mathf.Abs(mouseMove.x);
+                float Y = Mathf.Abs(mouseMove.y);
 
-            if(mouseMove.y < 0 && X < Y)
-            {
-                Debug.Log("Up");
+                if (mouseMove.y < 0 && X < Y)
+                {
+                    Debug.Log("Up");
+                }
+                else if (mouseMove.y > 0 && X < Y)
+                {
+                    Debug.Log("Down");
+                }
+                else if (mouseMove.x > 0 && X > Y)
+                {
+                    Debug.Log("Left");
+                }
+                else if (mouseMove.x < 0 && X > Y)
+                {
+                    Debug.Log("Right");
+                }
             }
-            else if(mouseMove.y > 0 && X < Y)
-            {
-                Debug.Log("Down");
-            }
-            else if(mouseMove.x > 0 && X > Y)
-            {
-                Debug.Log("Left");
-            }
-            else if(mouseMove.x < 0 && X > Y)
-            {
-                Debug.Log("Right");
-            }
+
+            check = 0;
         }
     }
 }
